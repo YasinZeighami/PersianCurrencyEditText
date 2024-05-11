63ad509dc4710814e34c39897ca49a0c482c269f
Yasin <yasin.zeighami@gmail.com>
2024-05-11 07:11:11 -0700

    initial commit

--- app/src/main/java/com/github/persiancurrencyedittext/MainActivity.kt
+++ app/src/main/java/com/github/persiancurrencyedittext/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.github.persiancurrencyedittext
 
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.github.currencyedittext.CurrencyEditText
 import com.github.currencyedittext.CurrencyType
 
